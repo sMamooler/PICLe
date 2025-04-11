311fa458a46247d3d4714ba97a5ba7b89aa779aa
sMamooler <s.mamooler@gmail.com>
2025-04-11 10:52:49 +0200

    added notebook for analysis with partially correct demonstrations

--- scripts/analysis_partially_correct_demos.sh
+++ scripts/analysis_partially_correct_demos.sh
@@ -6,7 +6,11 @@ do
     do
         for perturbation_factor in "01" "02" "03" "04" "05" "06" "07" "08" "09"
         do
-            python incontext_ned.py --config-path configs/analysis_partially_correct_demos/ data="$dataset" perturbation_type="$perturbation_type" perturbation_factor="$perturbation_factor"
+            python incontext_ned.py \
+             --config-path configs/analysis_partially_correct_demos/ \
+              data="$dataset" \
+              perturbation_type="$perturbation_type" \
+              perturbation_factor="$perturbation_factor"
         done
     done
 done
